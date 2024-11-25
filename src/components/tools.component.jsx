d1c15ed1fcbb53e6064d21d89b1fdfd7e09d8c18
Anmol Arora <heymailme04@gmail.com>
2024-11-25 14:57:06 +0530

    Update tools.component.jsx

--- src/components/tools.component.jsx
+++ src/components/tools.component.jsx
@@ -58,7 +58,7 @@ export const tools = {
         class: Header,
         config: {
             placeholder: "Type Heading....",
-            levels: [2, 3, 4, 5, 6],
+            levels: [2, 3],
             defaultLevel: 2
         }
     },
