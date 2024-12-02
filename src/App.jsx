d1ff04d216ddf4781563ee5ea3934a9c5226ba5c
Anmol Arora <heymailme04@gmail.com>
2024-12-02 13:26:00 +0530

    Lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar.component";
-import UserAuthForm from "./pages/userAuthForm.page";
-import { createContext, useEffect, useState } from "react";
+import React, { createContext, useEffect, useState, Suspense } from "react"; // Import Suspense here
 import { lookInSession } from "./common/session";
-import Editor from "./pages/editor.pages";
-import HomePage from "./pages/home.page";
-import SearchPage from "./pages/search.page";
-import PageNotFound from "./pages/404.page";
-import ProfilePage from "./pages/profile.page";
-import BlogPage from "./pages/blog.page";
-import SideNav from "./components/sidenavbar.component";
-import ChangePassword from "./pages/change-password.page";
-import EditProfile from "./pages/edit-profile.page";
-import Notifications from "./pages/notifications.page";
-import ManageBlogs from "./pages/manage-blogs.page";
-import HeroSection from "./components/hero.component";
-import AboutUsPage from "./pages/about-us.page";
+
+const Editor = React.lazy(() => import("./pages/editor.pages"));
+const HomePage = React.lazy(() => import("./pages/home.page"));
+const SearchPage = React.lazy(() => import("./pages/search.page"));
+const PageNotFound = React.lazy(() => import("./pages/404.page"));
+const ProfilePage = React.lazy(() => import("./pages/profile.page"));
+const BlogPage = React.lazy(() => import("./pages/blog.page"));
+const SideNav = React.lazy(() => import("./components/sidenavbar.component"));
+const ChangePassword = React.lazy(() => import("./pages/change-password.page"));
+const EditProfile = React.lazy(() => import("./pages/edit-profile.page"));
+const Notifications = React.lazy(() => import("./pages/notifications.page"));
+const ManageBlogs = React.lazy(() => import("./pages/manage-blogs.page"));
+const HeroSection = React.lazy(() => import("./components/hero.component"));
+const AboutUsPage = React.lazy(() => import("./pages/about-us.page"));
+const UserAuthForm = React.lazy(() => import("./pages/userAuthForm.page")); // Lazy-load UserAuthForm
+
+import Loader from "./components/loader.component";
 
 export const UserContext = createContext({});
 export const ThemeContext = createContext({});
@@ -54,29 +57,31 @@ const App = () => {
     return (
         <ThemeContext.Provider value={{ theme, setTheme }}>
             <UserContext.Provider value={{ userAuth, setUserAuth }}>
-                <Routes>
-                    <Route path="/editor" element={<Editor />} />
-                    <Route path="/editor/:blog_id" element={<Editor />} />
-                    <Route path="/" element={<Navbar />}> 
-                        <Route index element={<HeroSection />} />
-                        <Route path="/latest" element={<HomePage />} />
-                        <Route path="dashboard" element={<SideNav />} >
-                            <Route path="blogs" element={<ManageBlogs />} />
-                            <Route path="notifications" element={<Notifications />} />
+                <Suspense fallback={<Loader />}> {/* Use the Loader component here */}
+                    <Routes>
+                        <Route path="/editor" element={<Editor />} />
+                        <Route path="/editor/:blog_id" element={<Editor />} />
+                        <Route path="/" element={<Navbar />}> 
+                            <Route index element={<HeroSection />} />
+                            <Route path="/latest" element={<HomePage />} />
+                            <Route path="dashboard" element={<SideNav />} >
+                                <Route path="blogs" element={<ManageBlogs />} />
+                                <Route path="notifications" element={<Notifications />} />
+                            </Route>
+                            <Route path="settings" element={<SideNav />} >  
+                                <Route path="edit-profile" element={<EditProfile />} />
+                                <Route path="change-password" element={<ChangePassword />} />
+                            </Route>
+                            <Route path="signin" element={<UserAuthForm type="sign-in" />} /> 
+                            <Route path="signup" element={<UserAuthForm type="sign-up" />} />
+                            <Route path="search/:query" element={<SearchPage />} />
+                            <Route path="user/:id" element={<ProfilePage />} />
+                            <Route path="blog/:blog_id" element={<BlogPage />} />
+                            <Route path="/about-us" element={<AboutUsPage />} />
+                            <Route path="*" element={<PageNotFound />} /> 
                         </Route>
-                        <Route path="settings" element={<SideNav />} >  
-                            <Route path="edit-profile" element={<EditProfile />} />
-                            <Route path="change-password" element={<ChangePassword />} />
-                        </Route>
-                        <Route path="signin" element={<UserAuthForm type="sign-in" />} /> 
-                        <Route path="signup" element={<UserAuthForm type="sign-up" />} />
-                        <Route path="search/:query" element={<SearchPage />} />
-                        <Route path="user/:id" element={<ProfilePage />} />
-                        <Route path="blog/:blog_id" element={<BlogPage />}/>
-                        <Route path="/about-us" element={<AboutUsPage />}/>
-                        <Route path="*" element={<PageNotFound />} /> 
-                    </Route>
-                </Routes>
+                    </Routes>
+                </Suspense>
             </UserContext.Provider>
         </ThemeContext.Provider>
     );
